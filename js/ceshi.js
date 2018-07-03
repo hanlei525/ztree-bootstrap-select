667797d48c572872f9cbd00d87a519325ea9d2f3
hanlei525 <190182347@qq.com>
2018-07-03 19:23:17 +0800

    1

---
+++ js/ceshi.js
@@ -0,0 +1,13 @@
+var zNodes =[
+    {id:4, pId:0, name:"河北省", open:false, nocheck:true},
+    {id:41, pId:4, name:"石家庄"},
+    {id:411, pId:41, name:"石家庄1"},
+    {id:42, pId:4, name:"保定"},
+    {id:5, pId:0, name:"广东省", open:false, nocheck:true},
+    {id:51, pId:5, name:"广州"},
+    {id:52, pId:5, name:"深圳"},
+    {id:6, pId:0, name:"福建省", open:false, nocheck:true},
+    {id:61, pId:6, name:"福州"},
+    {id:62, pId:6, name:"厦门"},
+];
+
